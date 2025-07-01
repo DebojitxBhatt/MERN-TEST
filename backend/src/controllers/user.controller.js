d5507ec8701ccc47e3928a7ffa234ef0f91de6c2
debojit02 <181200050+DebojitxBhatt@users.noreply.github.com>
2025-07-01 19:02:30 +0530

    register logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,9 +2,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 //this method sends json response
 const registerUser = asyncHandler( async (req, res) => {
-        res.status(200).json({
-            message: "ok"
-        })
+         // get user details from frontend
+         //validation- not empty
+         // checking user in db if already exists with the help of username, email
+
+         //checking if the user provided files ( avatar // coverimage)
+         //creater user object - create entry in db
+         //remove password and refresher token fieild from response
+         // check for user creation
+         // return response
 } )
 
 
